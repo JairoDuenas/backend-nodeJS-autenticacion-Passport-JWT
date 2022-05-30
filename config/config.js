9d8acd589d64cd8380b05c23225a47237b2d2cb7
JairoDuenas <jairoduenas.ing@gmail.com>
2022-05-30 14:27:09 -0500

    📨 Envío de emails

--- config/config.js
+++ config/config.js
@@ -7,6 +7,8 @@ const config = {
   dbUrl: process.env.DATABASE_URL,
   apiKey: process.env.API_KEY,
   jwtSecret: process.env.JWT_SECRET,
+  emailSender: process.env.EMAIL_SENDER,
+  emailPassword: process.env.EMAIL_PASSWORD
 }
 
 module.exports = { config };
